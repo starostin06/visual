0d61079d86cbcd1d3f952db12f6d1b57cab5ef7f
Старостин Дмитрий <dima.starostin.2006@mail.ru>
2026-03-17 23:08:55 +0700

    feat: добавлены первые 7 типов (Transform, Where, Sort, Group, GroupBy, GroupTransform, Having)

---
+++ lab4.ts
@@ -0,0 +1,18 @@
+export type Transform<T> = (data: T[]) => T[];
+
+export type Where<T> = <K extends keyof T>(key: K, value: T[K]) => Transform<T>;
+
+export type Sort<T> = <K extends keyof T>(key: K) => Transform<T>;
+
+export type Group<T, K extends keyof T> = {
+  key: T[K];
+  items: T[];
+};
+
+export type GroupBy<T> = <K extends keyof T>(key: K) => Transform<Group<T, K>>;
+
+export type GroupTransform<T, K extends keyof T> = (groups: Group<T, K>[]) => Group<T, K>[];
+
+export type Having<T> = <K extends keyof T>(
+  predicate: (group: Group<T, K>) => boolean
+) => GroupTransform<T, K>;
